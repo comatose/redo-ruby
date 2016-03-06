477b14394d3ebffd87f8a19b6300f3a638fc5569
comatose <comatose>
2016-03-06 19:36:16 +0900

    Removed Redo class.

--- redo.rb
+++ redo.rb
@@ -7,16 +7,6 @@ require 'yaml'
 require 'digest'
 require 'base64'
 
-def list_do_files target
-  tokens = target.split('.')
-  results = []
-  for i in 1..tokens.length-1
-    results += [[tokens[0...i].join('.'), tokens[i...tokens.length]]]
-  end
-  [[target, "#{File.basename target}.do"]] + \
-  results.map { |basename, dofile| [basename, (["default"] + dofile + ["do"]).join('.')] }
-end
-
 module Settings
   REDO_CALL_DEPTH = "REDO_CALL_DEPTH"
   CALL_DEPTH = ENV.fetch(REDO_CALL_DEPTH, 0).to_i
@@ -112,77 +102,75 @@ class NonExistingDependency < Dependency
   end
 end
 
-class Redo
-  def initialize files
-    @targets = files.map { |f| redo_target_from_dir(Dir.pwd, f) }
-
+def list_do_files target
+  tokens = target.split('.')
+  results = []
+  for i in 1..tokens.length-1
+    results += [[tokens[0...i].join('.'), tokens[i...tokens.length]]]
   end
+  [[target, "#{File.basename target}.do"]] + \
+  results.map { |basename, dofile| [basename, (["default"] + dofile + ["do"]).join('.')] }
+end
 
-  def redo_target_from_dir(base_dir, target)
-    target = Pathname.new target
-    if target.relative?
-      return target.to_s
-    else
-      return target.relative_path_from(Pathname.new base_dir).to_s
-    end
+def target_from_dir(base_dir, target)
+  target = Pathname.new target
+  if target.relative?
+    return target.to_s
+  else
+    return target.relative_path_from(Pathname.new base_dir).to_s
   end
+end
 
-  def run
-    FileUtils.mkdir_p Settings::TEMP_OUT_DIR
-    FileUtils.mkdir_p Settings::DEPS_DIR
-    begin
-      @targets.each { |t| redo_for_ t }
-    rescue Exception => e
-      abort e.to_s
-    end
-    FileUtils.rm_rf Settings::TEMP_DIR
+def redo_ target
+  indent = ' ' * Settings::CALL_DEPTH
+  puts "visit #{indent + target}"
+  r = ExistingDependency.new(target, nil).up_to_date
+  puts "#{target} status = #{r}"
+  return if r == :uptodate
+
+  dofiles = list_do_files(target).select { |bn, df| File.exist? df }
+  abort "no dofiles found." if dofiles.empty?
+
+  run_do target, dofiles[0]
+
+  add_dependency Settings::DEPS_PATH, ExistingDependency.new(target, Settings::file_signature(target))
+end
+
+def add_dependency dep_file, dep
+  if dep_file
+    deps = YAML::load_file dep_file
+    deps = [] unless deps
+    deps << dep
+    File.open(dep_file, "w") { |file| file << YAML.dump(deps) }
   end
+end
 
-  def redo_for_ target
-    indent = ' ' * Settings::CALL_DEPTH
-    puts "visit #{indent + target}"
-    r = ExistingDependency.new(target, nil).up_to_date
-    puts "#{target} status = #{r}"
-    return if r == :uptodate
-
-    dofiles = list_do_files(target).select { |bn, df| File.exist? df }
-    abort "no dofiles found." if dofiles.empty?
-
-    run_do target, dofiles[0]
-
-    add_dependency Settings::DEPS_PATH, ExistingDependency.new(target, Settings::file_signature(target))
-  end
-
-  def add_dependency dep_file, dep
-    if dep_file
-      deps = YAML::load_file dep_file
-      deps = [] unless deps
-      deps << dep
-      File.open(dep_file, "w") { |file| file << YAML.dump(deps) }
-    end
-  end
-
-  def run_do target, dofile
-    tmp_out = File.join Settings::TEMP_DIR, target
-    tmp_deps = Tempfile.new target, Settings::TEMP_OUT_DIR
-    p "sh -vxe \"#{dofile[1]}\" \"#{dofile[0]}\" \"#{dofile[0]}\" \"#{tmp_out}\""
-    add_dependency tmp_deps, ExistingDependency.new(dofile[1], Settings::file_signature(dofile[1]))
-    r = system(Settings.spawned_env(tmp_deps.path), "sh", "-ve",
-               "#{dofile[1]}", "#{dofile[0]}", "#{dofile[0]}", "#{tmp_out}")
-    if r
-      FileUtils.mv tmp_out, target, :force => true
-      FileUtils.mv tmp_deps, File.join(Settings::DEPS_DIR, Settings.encode_path(target)), :force => true
-    else
-      FileUtils.rm_f [tmp_out, tmp_deps]
-      raise "#{dofile[1]} failed."
-    end
+def run_do target, dofile
+  tmp_out = File.join Settings::TEMP_DIR, target
+  tmp_deps = Tempfile.new target, Settings::TEMP_OUT_DIR
+  p "sh -vxe \"#{dofile[1]}\" \"#{dofile[0]}\" \"#{dofile[0]}\" \"#{tmp_out}\""
+  add_dependency tmp_deps, ExistingDependency.new(dofile[1], Settings::file_signature(dofile[1]))
+  r = system(Settings.spawned_env(tmp_deps.path), "sh", "-ve",
+             "#{dofile[1]}", "#{dofile[0]}", "#{dofile[0]}", "#{tmp_out}")
+  if r
+    FileUtils.mv tmp_out, target, :force => true
+    FileUtils.mv tmp_deps, File.join(Settings::DEPS_DIR, Settings.encode_path(target)), :force => true
+  else
+    FileUtils.rm_f [tmp_out, tmp_deps]
+    raise "#{dofile[1]} failed."
   end
 end
 
 case $0
 when /.*(redo)(-ifchange)*$/
-  redo_ = Redo.new ARGV
-  redo_.run
+  FileUtils.mkdir_p Settings::TEMP_OUT_DIR
+  FileUtils.mkdir_p Settings::DEPS_DIR
+  begin
+    ARGV.each { |f| redo_ target_from_dir(Dir.pwd, f) }
+  rescue Exception => e
+    abort e.to_s
+  end
+  FileUtils.rm_rf Settings::TEMP_DIR
 else
   abort "unknown redo command"
 end
